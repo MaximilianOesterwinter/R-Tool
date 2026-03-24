5ea5854f863b58736dea04c10b79108519f210b3
Maximilian Oesterwinter <m.oesterwinter@gmx.net>
2026-03-24 18:04:06 +0100

    Version: 0.9.6 Added Mann-Whitney-U-Test

--- R_Tool.py
+++ R_Tool.py
@@ -24,7 +24,15 @@ root.minsize(200, 200)
 root.maxsize(1280, 1080)
 root.geometry("800x800+50+50")
 
-two_variables_needed = ["chi_square", "paired_ttest", "unpaired_ttest", "norm_test", "welch_test", "describeBy", "correlation", "mann_whitney_test"]
+two_variables_needed = ["chi_square", 
+                        "paired_ttest", 
+                        "unpaired_ttest", 
+                        "norm_test", 
+                        "welch_test", 
+                        "describeBy", 
+                        "correlation", 
+                        "mann_whitney_test"
+                        ]
 more_variables_needed = ["logit", "lin_reg", "anova"]
 one_variable_needed = ["describe"]
 
